f096dc12c971df779c059d787da3aec1490fb7eb
Maximilian Walz <github@maximilian-walz.com>
2024-01-11 18:53:39 +0100

    Adjust page, post and footer layouts

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
 import { slug } from 'github-slugger'
+import { propagateServerField } from 'next/dist/server/lib/render-server'
 interface Props {
   text: string
+  leftAlign?: boolean
 }
 
-const Tag = ({ text }: Props) => {
+const Tag = ({ text, leftAlign=true }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className={(leftAlign ? "mr-3" : "ml-3") + " text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"}
     >
       {text.split(' ').join('-')}
     </Link>
