f930221e355b7ce289f49e97caec15509ae6352c
Maximilian Walz <github@maximilian-walz.com>
2024-01-11 18:52:33 +0100

    Add example post

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Appreciating the Analog - Creating a Custom Clock Face',
+    description: `
+      I like analog clocks. I know a lot of people who find them hard to read compared to their digital counterparts but to me they communicate time in a very distinct way. Growing up, a digital clock on every household appliance wasn't exactly the standard. So the analog clocks that were hanging in basically every room where the ones I learned to read first. And now, having gotten used to them over the years, I don't really have to parse the digits the clock hands are pointing to. I see the angle they're at and get a sense of time passed - just by giving it a quick glance.
+    `,
+    imgSrc: '/static/images/custom_clockface/disc_drawings.jpg',
+    href: '/blog/wooden_clockface',
   },
 ]
 
