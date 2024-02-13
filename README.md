5bdcc09ba175e7bc7a0fb87b14ff983c8ecb2d66
Maximilian Walz <github@maximilian-walz.com>
2024-02-13 14:38:41 +0100

    Fix formatting

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-   ![logo of my blog](/public/static/images/logo.png)
+![logo of my blog](/public/static/images/logo.png)
 
-   # My Personal Blog
+# My Personal Blog
 
-   This is the repo of my personal blog based on the [template](https://github.com/timlrx/tailwind-nextjs-starter-blog) provided by [timlrx](https://github.com/timlrx).
-   You can visit it [here](https://maximilian-walz.com).
+This is the repo of my personal blog based on the [template](https://github.com/timlrx/tailwind-nextjs-starter-blog) provided by [timlrx](https://github.com/timlrx).
+You can visit it [here](https://maximilian-walz.com).
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,14 +1,15 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
     title: 'Appreciating the Analog - Creating a Custom Clock Face',
-    description: 'I made a wooden clock face for an old clock clockwork, so I could finally have an analog clock in my room again.',
+    description:
+      'I made a wooden clock face for an old clock clockwork, so I could finally have an analog clock in my room again.',
     href: '/blog/wooden_clockface',
     imgSrc: '/static/images/custom_clockface/disc_drawings.jpg',
   },
@@ -16,20 +17,19 @@ const projectsData: Project[] = [
     title: 'Knit Bracelet',
     description: 'I knit a bracelet out of brass wire.',
     href: '/blog/knit_bracelet',
-    imgSrc: '/static/images/knit_bracelet/finished.jpg'
+    imgSrc: '/static/images/knit_bracelet/finished.jpg',
   },
   {
     title: 'Heart Pendant',
     description: 'I created a heart pendant out of two stones I found in a local quarry lake.',
     href: '/blog/heart_pendant',
-    imgSrc: '/static/images/heart_pendant.jpg'
+    imgSrc: '/static/images/heart_pendant.jpg',
   },
   {
     title: 'Companion Cube Pendant',
     description: 'I made a pendant resembling the Companion Cube from Portal.',
     href: '/blog/companion_cube_pendant',
-    imgSrc: '/static/images/companion_cube.jpg'
-
+    imgSrc: '/static/images/companion_cube.jpg',
   },
 ]
 
