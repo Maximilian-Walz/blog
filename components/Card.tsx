406cf92187863b3d689849c0c29976554c25bfa5
Maximilian Walz <github@maximilian-walz.com>
2024-02-13 17:04:30 +0100

    Redesign cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,24 @@
+import siteMetadata from '@/data/siteMetadata'
+import { formatDate } from 'pliny/utils/formatDate'
 import Image from './Image'
 import Link from './Link'
+import Tag from './Tag'
 
-const Card = ({ title, description, imgSrc, href }) => (
+type Props = {
+  title: string
+  description: string
+  imgSrc?: string
+  href?: string
+  tags?: string[]
+  date?: string
+}
+
+const Card = ({ title, description, imgSrc, href, tags, date }: Props) => (
   <div className="md max-w-[544px] p-4 md:w-1/2">
     <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      className={
+        'h-full overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700'
+      }
     >
       {imgSrc &&
         (href ? (
@@ -28,7 +40,16 @@ const Card = ({ title, description, imgSrc, href }) => (
             height={306}
           />
         ))}
-      <div className="p-6">
+      <div className="p-6 py-4">
+        <div className="mb-4">
+          {tags && (
+            <div className="flex flex-wrap">
+              {tags.map((tag) => (
+                <Tag key={tag} text={tag} />
+              ))}
+            </div>
+          )}
+        </div>
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
@@ -39,15 +60,6 @@ const Card = ({ title, description, imgSrc, href }) => (
           )}
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
-        )}
       </div>
     </div>
   </div>
