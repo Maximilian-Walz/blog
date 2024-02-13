1ba48b27df6fef4eda67bd7f582464eff5f258ca
Maximilian Walz <github@maximilian-walz.com>
2024-02-13 17:03:51 +0100

    Adjust tag colors

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -12,7 +12,7 @@ const Tag = ({ text, leftAlign = true }: Props) => {
       href={`/tags/${slug(text)}`}
       className={
         (leftAlign ? 'mr-3' : 'ml-3') +
-        ' me-2 rounded-full bg-primary-400 px-2.5 py-0.5 text-xs font-medium text-primary-600 hover:text-primary-700 dark:bg-primary-600 dark:text-primary-400 dark:hover:text-primary-300'
+        ' me-2 rounded-full bg-primary-400 px-2.5 py-0.5 text-xs font-medium text-primary-600 hover:text-primary-700 dark:bg-primary-700 dark:text-primary-500 dark:hover:text-primary-400'
       }
     >
       {text.split(' ').join('-')}
