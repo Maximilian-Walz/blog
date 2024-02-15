cc98239bd0d7b42f583b0e7fddae4838938139c9
Maximilian Walz <github@maximilian-walz.com>
2024-02-15 18:48:04 +0100

    Fix projects page pagination

--- app/projects/page/[page]/page.tsx
+++ app/projects/page/[page]/page.tsx
@@ -5,11 +5,18 @@ import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 
 const POSTS_PER_PAGE = 5
 
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+
+  return paths
+}
+
 export const metadata = genPageMetadata({ title: 'Projects' })
 
-export default function ProjectsPage() {
+export default function Page({ params }: { params: { page: string } }) {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
+  const pageNumber = parseInt(params.page as string)
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
     POSTS_PER_PAGE * pageNumber
