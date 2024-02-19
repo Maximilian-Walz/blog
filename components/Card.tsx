39ce860ff6275eedb94816f11bb04535d227b29c
Maximilian Walz <github@maximilian-walz.com>
2024-02-19 22:02:07 +0100

    Redesign card

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,53 +13,47 @@ type Props = {
   date?: string
 }
 
+const image = (title, imgSrc) => (
+  <Image
+    alt={title}
+    src={imgSrc}
+    className="object-cover object-center transition-all duration-500 hover:scale-110"
+    width={544}
+    height={306}
+  />
+)
+
 const Card = ({ title, description, imgSrc, href, tags, date }: Props) => (
-  <div className="md max-w-[544px] p-4 md:w-1/2">
-    <div
-      className={
-        'h-full overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700'
-      }
-    >
+  <div className="p-6 md:w-1/2">
+    <div className="overflow-hidden rounded-xl">
       {imgSrc &&
         (href ? (
           <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center"
-              width={544}
-              height={306}
-            />
+            {image(title, imgSrc)}
           </Link>
         ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center"
-            width={544}
-            height={306}
-          />
+          image(title, imgSrc)
         ))}
-      <div className="p-6 py-4">
-        <div className="mb-4">
-          {tags && (
-            <div className="flex flex-wrap">
-              {tags.map((tag) => (
-                <Tag key={tag} text={tag} />
-              ))}
-            </div>
-          )}
-        </div>
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
-        </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+    </div>
+    <div className="p-4">
+      <h2 className="text-2xl font-bold leading-8 tracking-tight">
+        {href ? (
+          <Link href={href} aria-label={`Link to ${title}`}>
+            {title}
+          </Link>
+        ) : (
+          title
+        )}
+      </h2>
+      {<p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>}
+      <div className="my-2">
+        {tags && (
+          <div className="flex flex-wrap">
+            {tags.map((tag) => (
+              <Tag key={tag} text={tag} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   </div>
