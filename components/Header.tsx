9c869913b16a9d8f00a841a5608fe3ef4cff8d96
Maximilian Walz <github@maximilian-walz.com>
2024-02-21 17:00:41 +0100

    Animate underline on active navbar link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,43 @@
-import siteMetadata from '@/data/siteMetadata'
+'use client'
 import headerNavLinks from '@/data/headerNavLinks'
+import siteMetadata from '@/data/siteMetadata'
 import Link from './Link'
 import Logo from './Logo'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import ThemeSwitch from './ThemeSwitch'
+import { motion } from 'framer-motion'
+
+import { usePathname } from 'next/navigation'
+import { useState } from 'react'
+
+function NavLink({ href, title }) {
+  const [hovering, setHovering] = useState(false)
+  const pathname = usePathname()
+
+  const active = pathname === href
+
+  return (
+    <motion.div onHoverStart={() => setHovering(true)} onHoverEnd={() => setHovering(false)}>
+      <Link
+        href={href}
+        className="relative hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500 sm:block"
+      >
+        {title}
+        {active && (
+          <motion.div
+            className={`absolute inset-0 bottom-0 text-transparent underline ${
+              hovering ? 'decoration-primary-500' : 'decoration-gray-900 dark:decoration-gray-100'
+            }`}
+            layoutId="underline"
+          >
+            {title}
+          </motion.div>
+        )}
+      </Link>
+    </motion.div>
+  )
+}
 
 const Header = () => {
   return (
@@ -29,13 +62,7 @@ const Header = () => {
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500 sm:block"
-            >
-              {link.title}
-            </Link>
+            <NavLink key={link.href} {...link} />
           ))}
         <SearchButton />
         <ThemeSwitch />
