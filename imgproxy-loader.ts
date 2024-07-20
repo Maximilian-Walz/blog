ef1089cda30ffd61c928bd2b8b040c4ae6b9632a
Maximilian Walz <github@maximilian-walz.com>
2024-07-20 20:09:48 +0200

    Use webp as default for images

--- imgproxy-loader.ts
+++ imgproxy-loader.ts
@@ -8,5 +8,5 @@ type ImgproxyProps = {
 export default function imgproxyLoader({ src, width, height, quality }: ImgproxyProps) {
   width ??= 0
   height ??= 0
-  return `https://images.maximilian-walz.com/insecure/rs:fill:${width}:${height}/g:sm/plain/local:///${src}`
+  return `https://images.maximilian-walz.com/insecure/rs:fill:${width}:${height}/g:sm/plain/local:///${src}@webp`
 }
